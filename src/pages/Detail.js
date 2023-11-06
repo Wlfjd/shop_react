c78901ecdf86caa1d721d230254ed78ae34fe3ba
Wlfjd <leejiwon813@naver.com>
2023-11-06 16:31:24 +0900

    2초 뒤 사라지는 알람창 구현

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,20 +1,31 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export function Detail({data}){
   let {id}= useParams()
+  let [show,setShow]=useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+     setShow(false)
+    },2000)
+  })
     return(
+      <>
+        {show && <div className="alert alert-warning">2초 이내 구매시 할인</div>}
       <div className="container">
-    <div className="row">
-      <div className="col-md-6">
-        <img src={data[id].img} alt="img" width="100%" />
-      </div>
-      <div className="col-md-6">
-        <h4 className="pt-5">{data[id].title}</h4>
-        <p>{data[id].content}</p>
-        <p>{data[id].price}원</p>
-        <button className="btn btn-danger">주문하기</button> 
-      </div>
-    </div>
-  </div> 
+        <div className="row">
+          <div className="col-md-6">
+            <img src={data[id].img} alt="img" width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{data[id].title}</h4>
+            <p>{data[id].content}</p>
+            <p>{data[id].price}원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
+      </>
     )
   }
