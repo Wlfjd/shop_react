51295a9d20516d9641352f3792c5b35652f8f585
Wlfjd <leejiwon813@naver.com>
2023-11-08 15:59:54 +0900

    tab UI 구현

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useEffect, useState } from "react"
+import { Nav } from 'react-bootstrap'
 import { useParams } from "react-router-dom"
 
 
 export function Detail({data}){
   let {id}= useParams()
   let [show,setShow]=useState(true)
+  let [tab,setTab]=useState(0)
 
   useEffect(()=>{
     setTimeout(()=>{
@@ -28,6 +30,22 @@ export function Detail({data}){
           </div>
         </div>
       </div> 
+      <Nav variant="tabs" className="my-5" fill defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link onClick={()=>setTab(0)} eventKey="link0">상세정보</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>setTab(1)} eventKey="link1">후기</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>setTab(2)} eventKey="link2">질문</Nav.Link>
+          </Nav.Item>
+      </Nav>
+      <TabContent tab={tab}/>
       </>
     )
+  }
+
+  function TabContent({tab}){
+    return [<div>상세정보입니다</div>,<div>구매 후기입니다</div>,<div>질문 게시판입니다</div>][tab]
   }
