9687711302ab31fd939ab1af2c1749ff3d1c8d65
Wlfjd <leejiwon813@naver.com>
2023-11-21 16:17:40 +0900

    react-query 우측 하단 아이콘 제거

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query' 
 import { Provider } from 'react-redux';
 import store from './store';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 
 const queryClient= new QueryClient()
@@ -15,13 +14,13 @@ const queryClient= new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-     <ReactQueryDevtools initialIsOpen={true} />
     <Provider store={store}>
       <BrowserRouter>
       <App />
       </BrowserRouter>
     </Provider>
   </QueryClientProvider>
+
 
 );
 
